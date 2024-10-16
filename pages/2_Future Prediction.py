46b5b9cd375708babe38aa7d9b26c027b4c4e20f
Michael Wong <michael.wong@dxc.com>
2024-10-16 16:45:17 +0800

    update

--- pages/2_Future Prediction.py
+++ pages/2_Future Prediction.py
@@ -2,9 +2,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import squarify
-import matplotlib
 from controller import controllerService
 
 # Set page config
