93607e1503b34b3d2473a5c24f51877b211e37be
Michael Wong <michael.wong@dxc.com>
2024-10-16 11:57:29 +0800

    Update sentimentExtraction.py

--- models/sentimentExtraction/sentimentExtraction.py
+++ models/sentimentExtraction/sentimentExtraction.py
@@ -9,9 +9,9 @@ def sentimentExtraction(self, aspectInput_df):
     df = aspectInput_df
 
     # ... run sentiment model for each sentence (containing 1 aspect) to obtain sentiment
-    aspectSentimentOutput_df = df.copy()
+    aspectSentimentOutput_df = df
      
     # ... aggregate results into final results output
-    overallResultsOutput_df = aspectSentimentOutput_df.copy()
+    overallResultsOutput_df = aspectSentimentOutput_df
     
     return aspectSentimentOutput_df, overallResultsOutput_df # aspect-sentiment result outputs; aggregated final outputs
