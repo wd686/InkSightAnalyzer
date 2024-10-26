11104184ed1cc75900cb389a0f03b2d296bb14ad
Michael Wong <michael.wong@dxc.com>
2024-10-26 11:54:50 +0800

    Update 2_Sentiment Analyzer.py

--- pages/2_Sentiment Analyzer.py
+++ pages/2_Sentiment Analyzer.py
@@ -188,5 +188,5 @@ try:
         st.write("Uploaded CSV file is not formatted correctly. Please include only 2 columns and label them as 'Time period' and 'Reviews'.")
 
 except Exception as e:
-    # pass
-    st.write(e)
+    pass
+    # st.write(e)
